5d6c08b5e9bd7632c8910c8dc528633b2d75a6eb
Joel Cressy <joel@jtcressy.net>
2017-10-26 01:21:44 -0600

    dial down the tickrate

--- multipong/game.py
+++ multipong/game.py
@@ -2,7 +2,7 @@ from multipong import socketio
 import time
 
 
-def game_loop(tickrate=60):
+def game_loop(tickrate=1):
     print("started game loop")
     while True:
         time.sleep(1/tickrate)
