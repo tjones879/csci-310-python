ead7fdc9c194393e8e6a686ab90317ccb45caeb8
Joel Cressy <joel@jtcressy.net>
2017-11-08 15:34:01 -0700

    FIX: rooms now being created with unique UUID's Use ``models.<model>.new()`` to create a new unique object. Do NOT use ``models.<model>.create()``

--- multipong/models.py
+++ multipong/models.py
@@ -1,12 +1,27 @@
-from multipong import app, walrus_conn
+from multipong import walrus_conn
 import walrus
 import uuid
+import random
+
+
+DEFAULT_ARENA_SIZE = 1000
+BALL_TYPES = ["normal"]
+
 
 
 class Room(walrus.Model):
+    @staticmethod
+    def new():
+        room = Room.create(
+            id=uuid.uuid4(),
+            arenasize=DEFAULT_ARENA_SIZE
+        )
+        room.save()
+        return room
+
     __database__ = walrus_conn
     id = walrus.UUIDField(default=str(uuid.uuid4()), index=True)
-    balls = walrus.SetField()  # [ <uuid>, ... ]
+    balls = walrus.ListField()  # [ <uuid>, ... ]
     players = walrus.SetField()  # [ <uuid>, ... ]
     spectators = walrus.SetField()  # [ <uuid>, ... ]
     # [ {playerid: <uuid>, score: <int>}, ... ]
@@ -15,17 +30,40 @@ class Room(walrus.Model):
 
 
 class Ball(walrus.Model):
+    @staticmethod
+    def new():
+        ball = Ball.create(
+            id=uuid.uuid4(),
+            position={
+                'x': 500,
+                'y': 500
+            },
+            vector={
+                'x': 50,
+                'y': 50
+            },
+            ballType=random.choice(BALL_TYPES)
+        )
+        ball.save()
+        return ball
     __database__ = walrus_conn
     id = walrus.UUIDField(default=str(uuid.uuid4()), index=True)
-    position = walrus.HashField()
-    vector = walrus.HashField()
+    position = walrus.Field()
+    vector = walrus.Field()
     ballType = walrus.TextField()
 
 
 class Player(walrus.Model):
+    @staticmethod
+    def new():
+        player = Player.create(
+            id=uuid.uuid4()
+        )
+        player.save()
+        return player
     __database__ = walrus_conn
     id = walrus.UUIDField(default=str(uuid.uuid4()), index=True)
-    sid = walrus.UUIDField()  # uuid.UUID(session.sid
+    sid = walrus.UUIDField()  # uuid.UUID(session.sid)
     room = walrus.UUIDField()  # <uuid>
     paddle = walrus.HashField()  # {pos: <int>, width: <int>}
     username = walrus.TextField()  # <str>
