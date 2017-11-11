6a8845a3bd9747cd61814e5555b201b9869afe49
Tyler Jones <tdjones879@gmail.com>
2017-11-11 12:11:36 -0700

    Clean up model tests with pytest fixtures

--- multipong/tests/test_models.py
+++ multipong/tests/test_models.py
@@ -1,69 +1,74 @@
 from multipong import models
 import uuid
+import pytest
+
 
 class TestRoom:
-    def test_create(self):
+    @pytest.fixture(scope='function')
+    def room(self):
         room = models.Room.new()
+        yield room
+        room.delete()
+
+    def test_create(self, room):
         room.balls.append(models.Ball.new().id)
         assert isinstance(room.id, uuid.UUID), "room.id is actually of type {}".format(str(type(room.id)))
         assert isinstance(uuid.UUID(bytes.decode(list(room.balls)[0], 'utf-8')), uuid.UUID), "stored ball id is not a valid UUID"
         assert room.arenasize == models.DEFAULT_ARENA_SIZE
         room.delete()
 
-    def test_playeradd(self):
-        room = models.Room.new()
+    def test_playeradd(self, room):
         assert room.players.add("testplayer")
         room.save()
         assert "testplayer" in room.players
         room.players.remove("testplayer")
         assert "testplayer" not in room.players
-        room.delete()
 
-    def test_specadd(self):
-        room = models.Room.new()
+    def test_specadd(self, room):
         assert room.spectators.add("testspectator")
         room.save()
         assert "testspectator" in room.spectators
         room.spectators.remove("testspectator")
         assert "testspectator" not in room.spectators
-        room.delete()
 
-    def test_uniqueness(self):
-        room1 = models.Room.new()
+    def test_uniqueness(self, room):
         room2 = models.Room.new()
-        assert room1.id != room2.id
-        room1.delete()
+        assert room.id != room2.id
         room2.delete()
 
 
 class TestBall:
-    def test_create(self):
+    @pytest.fixture(scope='function')
+    def ball(self):
         ball = models.Ball.new()
+        yield ball
+        ball.delete()
+
+    def test_create(self, ball):
         assert isinstance(ball.id, uuid.UUID)
         assert ball.position['x'] == 500
         assert ball.position['y'] == 500
         assert ball.vector['x'] == 50
         assert ball.vector['y'] == 50
         assert ball.ballType in models.BALL_TYPES
-        ball.delete()
 
-    def test_uniqueness(self):
-        ball1 = models.Ball.new()
+    def test_uniqueness(self, ball):
         ball2 = models.Ball.new()
-        assert ball1.id != ball2.id
-        ball1.delete()
+        assert ball.id != ball2.id
         ball2.delete()
 
 
 class TestPlayer:
-    def test_create(self):
+    @pytest.fixture(scope='function')
+    def player(self):
         player = models.Player.new()
-        assert isinstance(player.id, uuid.UUID)
+        yield player
         player.delete()
 
-    def test_uniqueness(self):
-        player1 = models.Player.new()
+    def test_create(self, player):
+        assert isinstance(player.id, uuid.UUID)
+
+    def test_uniqueness(self, player):
         player2 = models.Player.new()
-        assert player1.id != player2.id
-        player1.delete()
+        assert player.id != player2.id
         player2.delete()
